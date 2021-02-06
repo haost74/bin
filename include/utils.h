900111e66633f2f62c788c25d46747fdf8e3e264
haost <Aleksey.V.Gribkov@rt.ru>
2021-02-06 12:25:02 +0300

    add utils

---
+++ include/utils.h
@@ -0,0 +1,29 @@
+#ifndef __UTILS__CPP__
+#define __UTILS__CPP__
+
+#include <unistd.h>
+#include <string>
+#include <vector>
+
+namespace binance
+{
+    void split_string( std::string &s, char delim, std::vector<std::string> &result);
+	bool replace_string( std::string& str, const char *from, const char *to);
+	int replace_string_once( std::string& str, const char *from, const char *to , int offset);
+
+	std::string b2a_hex( char *byte_arr, int n );
+	time_t get_current_epoch();
+	unsigned long get_current_ms_epoch();
+
+	inline bool file_exists (const std::string& name)
+	{
+	 	 return ( access( name.c_str(), F_OK ) != -1 );
+	}
+
+	std::string hmac_sha256( const char *key, const char *data);
+	std::string sha256( const char *data );
+	void string_toupper( std::string &src);
+	std::string string_toupper( const char *cstr );
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //#include "include/logger.h"
 
 #include "logger.h"
+#include "utils.h"
 
 
 int main(int argc, char**args){
@@ -12,5 +13,12 @@ std::cout << 10 << '\n';
 binance::logger::set_debug_level(3);
 
 binance::logger::write_log("<get_account>");
+
+//---------------------------------------------
+
+std::cout << binance::sha256("hello") << '\n';
+
+//---------------------------------------------
+
     return 0;
 }
